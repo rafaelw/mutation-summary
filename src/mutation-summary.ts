de7f2ecabf5f2ef2f513c30a264ca2277835d574
Bruce Krysiak <brucek@alum.mit.edu>
2015-04-30 00:09:21 -0700

    Needed this to compile using tsc

--- src/mutation-summary.ts
+++ src/mutation-summary.ts
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+declare var WebKitMutationObserver: any;
+
 var MutationObserverCtor;
 if (typeof WebKitMutationObserver !== 'undefined')
   MutationObserverCtor = WebKitMutationObserver;
